702df0c643be88f6833a10d7c81f06fe5d3e1bb7
SarahHanks04 <sarahhanks04@gmail.com>
2024-11-18 16:15:47 +0100

    updated

--- src/Feedback/FeedbackFormConfig.jsx
+++ src/Feedback/FeedbackFormConfig.jsx
@@ -24,6 +24,41 @@
 // };
 
 
+
+// SECOND TRY
+
+
+// import * as Yup from "yup";
+
+// export const initialValues = {
+//   satisfaction: "",
+//   emojis: "",
+//   reasons: [],
+// };
+
+// export const validationSchema = Yup.object({
+//   satisfaction: Yup.string().required("This field is required"),
+//   emojis: Yup.string().required("This field is required"),
+//   reasons: Yup.array().min(1, "At least one reason is required"),
+// });
+
+// export const handleSubmit = (
+//   values,
+//   { resetForm },
+//   saveFormData,
+//   setModalIsOpen
+// ) => {
+//   saveFormData("feedback", values);
+//   setModalIsOpen(true);
+//   resetForm();
+// };
+
+
+
+
+// THIRD TRY
+
+
 import * as Yup from "yup";
 
 export const initialValues = {
@@ -41,10 +76,13 @@ export const validationSchema = Yup.object({
 export const handleSubmit = (
   values,
   { resetForm },
+  actions,
   saveFormData,
   setModalIsOpen
 ) => {
+  console.log(actions)
   saveFormData("feedback", values);
+  actions.setSubmitting(false);
   setModalIsOpen(true);
   resetForm();
 };
