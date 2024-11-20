3ac60030a3e0d0ebbe4e6f37df0296fe99a650d6
SarahHanks04 <sarahhanks04@gmail.com>
2024-11-20 01:11:45 +0100

    fix: local date

--- src/Context/CapstoneContext.jsx
+++ src/Context/CapstoneContext.jsx
@@ -82,47 +82,8 @@
 
 // export default CapstoneProvider;
 
-// SECOND TRY WITH API
-import React, { createContext, useState } from "react";
-import axios from "axios";
+// 
 
-export const CapstoneContext = createContext();
 
-const CapstoneProvider = ({ children }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  // Save FormData to the JSON server
-  const saveFormData = async (type, data) => {
-    const currentDate = new Date().toLocaleDateString("en-US", {
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-    });
 
-    const newData = { ...data, date: currentDate };
-
-    try {
-      if (type === "feedback") {
-        await axios.post("http://localhost:4000/feedback", newData);
-      } else if (type === "complaints") {
-        await axios.post("http://localhost:4000/complaint", newData);
-      }
-    } catch (error) {
-      console.error("Error saving data:", error);
-    }
-  };
-
-  const values = {
-    saveFormData,
-    modalIsOpen,
-    setModalIsOpen,
-  };
-
-  return (
-    <CapstoneContext.Provider value={values}>
-      {children}
-    </CapstoneContext.Provider>
-  );
-};
-
-export default CapstoneProvider;
